d3f7467c08f7eaace9b973f03f14d9f864ca30a5
Ricardo Chagas Rapacki <rcrapacki@gmail.com>
2017-04-30 16:34:34 -0300

    adding task again

--- Gruntfile.js
+++ Gruntfile.js
@@ -828,5 +828,5 @@ module.exports = function (grunt) {
     'build'
   ]);
 
-  grunt.registerTask('heroku:production', '');
+  grunt.registerTask('heroku:production', 'clean less mincss uglify');
 };
